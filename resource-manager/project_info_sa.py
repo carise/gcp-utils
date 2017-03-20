506d9aac679d685e162514db30536f1dd1bf3f42
Carise Fernandez <carise@google.com>
2017-03-20 14:58:48 -0700

    ratelimiter

--- resource-manager/project_info_sa.py
+++ resource-manager/project_info_sa.py
@@ -8,6 +8,7 @@
 
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
+from ratelimiter import RateLimiter
 
 def resource_mgr_service():
     credentials = GoogleCredentials.get_application_default()
@@ -18,23 +19,31 @@ def resources_projects(service):
     return projects
 
 def main():
-    projects_api = resources_projects(resource_mgr_service())
-    proj_request = projects_api.list()
-    while proj_request is not None:
-        response = proj_request.execute()
-        for project in response['projects']:
-            policy_req = projects_api.getIamPolicy(
-                resource=project['projectId'], body={})
-            policy_res = policy_req.execute()
-            num_bindings = 0
-            if 'bindings' in policy_res:
-              num_bindings = len(policy_res['bindings'])
+    rate_limiter = RateLimiter(400, 100)
 
-            print '{} ({}): {} IAM bindings'.format(
-                project['projectId'], project['projectNumber'], num_bindings)
+    with rate_limiter:
+        projects_api = resources_projects(resource_mgr_service())
+        proj_request = projects_api.list()
+        while proj_request is not None:
+            response = proj_request.execute()
+            for project in response['projects']:
+                try:
+                    policy_req = projects_api.getIamPolicy(
+                        resource=project['projectId'], body={})
+                    policy_res = policy_req.execute()
+                    num_bindings = 0
+                    if 'bindings' in policy_res:
+                        num_bindings = len(policy_res['bindings'])
 
-        proj_request = projects_api.list_next(
-            previous_request=proj_request, previous_response=response)
+                    print '{} ({}): {} IAM bindings'.format(
+                        project['projectId'],
+                        project['projectNumber'],
+                        num_bindings)
+                except:
+                    print 'Error getting IAM policy for {}'.format(project)
+
+            proj_request = projects_api.list_next(
+                previous_request=proj_request, previous_response=response)
 
 if __name__ == '__main__':
     main()
